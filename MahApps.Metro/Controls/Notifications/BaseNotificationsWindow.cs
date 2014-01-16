39a421c36326e1ddd5585f24ffc762807f6ce24a
Amrykid <amrykid@gmail.com>
2014-01-16 16:07:45 -0600

    Something that I've kepted on hold for a long time.

---
+++ MahApps.Metro/Controls/Notifications/BaseNotificationsWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace MahApps.Metro.Controls.Notifications
+{
+    public abstract class BaseNotificationsWindow: Window
+    {
+        static BaseNotificationsWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BaseNotificationsWindow), new FrameworkPropertyMetadata(typeof(BaseNotificationsWindow)));
+        }
+
+        public static readonly DependencyProperty BodyProperty = DependencyProperty.Register("Body", typeof(object), typeof(BaseNotificationsWindow), new PropertyMetadata(null));
+        /// <summary>
+        /// Gets/sets arbitrary content in the body of the notification window. 
+        /// </summary>
+        public object Body
+        {
+            get { return GetValue(BodyProperty); }
+            set { SetValue(BodyProperty, value); }
+        }
+    }
+}
